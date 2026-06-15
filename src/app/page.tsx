5f4bc0ae16f9e2ccb529703173ed33e5d6c36d67
Cesar Betancourt <cessarr862@gmail.com>
2026-06-14 21:13:44 -0600

    add: ripple and providers component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import RippleEffect from "@/components/RippleEffect";
+import { Button } from "@heroui/react";
 
 export default function Home() {
   return (
     <div>
       <h2 className="text-2xl text-red-600">Hello and welcome to lingochat!</h2>
+      <Button variant="primary"><RippleEffect/>Click me</Button>
     </div>
   );
 }
---
+++ src/components/RippleEffect.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { Ripple } from "m3-ripple";
+import 'm3-ripple/ripple.css';
+
+export default function RippleEffect() {
+  return (
+    <Ripple/>
+  )
+}
